b43c89c59e53ab21a570b88b00365db0bc8d4a42
Nadir Elserhid <nadirelserhid@gmail.com>
2026-05-11 22:15:35 +0300

    level 4 lesson 4

--- level-4/lesson_4/bfs.cpp
+++ level-4/lesson_4/bfs.cpp
@@ -1,27 +1,62 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
 
 int n;
-vector<int> g[n];
+vector<vector<int>> g;
+vector<int> dist;
 
-vector<int>dist(n,-1);
-queue<int> q;
-
-
-
-int main(){
-    int s = 0;
+void bfs(int s) {
     dist[s] = 0;
+    queue<int> q;
     q.push(s);
 
-    while(!q.empty()){
+    while (!q.empty()) {
         int v = q.front();
         q.pop();
-        for (int to : g[v]){
-            if(dist[to]!= -1) continue;
-            dist[to] = dist[v]+1;
+
+        for (int to : g[v]) {
+
+            if (dist[to] != -1) continue;
+
+            dist[to] = dist[v] + 1;
             q.push(to);
         }
     }
-
-return 0;
 }
+
+int main() {
+    int m;
+
+     cin >> n >> m;
+
+    g.resize(n);
+    dist.assign(n, -1);
+
+
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+
+    bfs(0);
+
+
+    for (int i = 0; i < n; ++i) {
+        cout << "Distance to node " << i << ": " << dist[i] << "\n";
+    }
+
+    return 0;
+}
+
+
+
+
+
