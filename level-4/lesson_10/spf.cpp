f58888426e9cdf1091555c8d1e93e3c0d8f782fe
Nadir Elserhid <nadirelserhid@gmail.com>
2026-05-15 16:12:09 +0300

    lesson 10  level 4

--- level-4/lesson_10/spf.cpp
+++ level-4/lesson_10/spf.cpp
@@ -5,18 +5,18 @@ using namespace std;
 const int max_n = 10'000'000;
 int spf[max_n + 1];
 
-// FIX 4: Changed 'int' to 'void' since we don't return anything
+
 void sieve(int n) {
-    // FIX (Optimization): Only initialize up to 'n'
+
     for (int i = 1; i <= n; i++) {
         spf[i] = i;
     }
 
     for (int i = 2; i * i <= n; i++) {
-        // FIX 1: Use == for comparison, not =
+
         if (spf[i] == i) {
             for (long long j = i * i; j <= n; j += i) {
-                // FIX 1: Use == for comparison
+
                 if (spf[j] == j) {
                     spf[j] = i;
                 }
@@ -25,16 +25,24 @@ void sieve(int n) {
     }
 }
 
-
+vector<int> getFactors(int x) {
+    vector<int> ret;
+    while (x != 1) {
+        ret.push_back(spf[x]);  // أضف أصغر عامل أولي
+        x = x / spf[x];          // اقسم وتابع
+    }
+    return ret;
+}
 
 int main() {
+        ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
     int n;
     cin >> n;
 
-    // FIX 2: You have to call the function to calculate the values!
     sieve(n);
 
-    // FIX 3: Initialize i = 2 (since SPF of 0 and 1 isn't very useful)
+
     for (int i = 2; i <= n; i++) {
         cout << "Smallest Prime Factor of " << i << " is: " << spf[i] << "\t";
     }
---
+++ level-4/problems/path_(2).cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<string> grid(n);
+    for (auto& row : grid) cin >> row;
+
+    vector<vector<int>> dist(n, vector<int>(m, -1));
+    queue<pair<int,int>> q;
+
+    dist[0][0] = 0;
+    q.push({0, 0});
+
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m
+                && grid[nx][ny] == '.' && dist[nx][ny] == -1) {
+                dist[nx][ny] = dist[x][y] + 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+
+    cout << dist[n-1][m-1] << "\n";
+    return 0;
+
+    }
